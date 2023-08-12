c5c0034628453ff78db213a11bc40c86e452b854
Gerson <grs@cesar.org.br>
2023-08-12 11:25:17 -0300

    Update solution.py

--- Iniciante/python-solution/1010-calculo-simples/solution.py
+++ Iniciante/python-solution/1010-calculo-simples/solution.py
@@ -1,13 +1,6 @@
-l1 = input().split()
-c1 = int(l1[0])
-q1 = int(l1[1])
-p1 = float(l1[2])
+code1, qtd1, valor1 = list(map(float, input().split()))
+code2, qtd2, valor2 = list(map(float, input().split()))
 
-l2 = input().split()
-c2 = int(l2[0])
-q2 = int(l2[1])
-p2 = float(l2[2])
-
-valor = q1 * p1 + q2 * p2
+valor = qtd1 * valor1 + qtd2 * valor2
 
 print(f"VALOR A PAGAR: R$ {valor:.2f}")
